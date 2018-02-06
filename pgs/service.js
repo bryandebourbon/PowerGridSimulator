30bbedc3533deae8202d4c3ef3f339e8e5094d86
Annie <annie.shenglin.yang@gmail.com>
2018-02-05 19:39:09 -0500

    initiated angular structure

---
+++ pgs/service.js
@@ -0,0 +1,32 @@
+app.service('LoginService', function () {
+	var init = function () {
+		console.log('Login service initiated.');
+	}
+
+
+	this.init = init;
+})
+
+app.service('GridService', function () {
+	var init = function () {
+		console.log('Grid service initiated.');
+	}
+
+	this.init = init;
+})
+
+app.service('EvaluationService', function () {
+	var init = function () {
+		console.log('Evaluation service initiated.');
+	}
+
+	this.init = init;
+})
+
+app.service('LeaderBoardService', function () {
+	var init = function () {
+		console.log('Leader board service initiated.');
+	}
+
+	this.init = init;
+})
