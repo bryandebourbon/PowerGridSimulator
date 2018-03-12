a036705257e873fbf0e3a4ce3124d036cd2981a1
Jane Liu <jane.liu.cn@gmail.com>
2018-03-11 21:51:52 -0400

    Read solar and wind output from csv; remove timestep_count hardcode

--- flask-api/pgsim/ppc_utils.py
+++ flask-api/pgsim/ppc_utils.py
@@ -69,47 +69,58 @@ gen_baseline = np.array(
 
 # Hardcoded generator parameters for each generator type over a time series.
 # Cost is in thousands, power is in 100MW, CO2 is in tonnes.
-# TODO: Read these from csv or database (?) and over longer time series.
+# TODO: Read these over longer time series.
 # TODO: Consider making the capacity location-dependent; e.g. not all 
 # locations can support a lot of hydro generation.
 # TODO: Consider making the cost time-dependent; e.g. cheaper to generator at night.
 gen_types = {
-    "G":   {"real_capacity": np.full((6), 25.0), 
-            "reactive_capacity": np.zeros(6),
+    "G":   {"real_capacity": np.full((timestep_count), 25.0), 
+            "reactive_capacity": np.zeros(timestep_count),
             "real_cost": np.array([2, 0., 0., 2, 15.6, 0]),
             "installation_cost": 2400000,
             "unit_CO2": 49.9, 
             "count": 10,
             "per_node_limit": {node:10 for node in range(10)}},
-    "H":   {"real_capacity": np.full((6), 12.0), 
-            "reactive_capacity": np.zeros(6),
+    "H":   {"real_capacity": np.full((timestep_count), 12.0), 
+            "reactive_capacity": np.zeros(timestep_count),
             "real_cost": np.array([2, 0., 0., 2, 4.3, 0]),
             "installation_cost": 2750000,
             "unit_CO2": 2.6, 
             "count": 10,
             "per_node_limit": {0:1, 1:2, 2:0, 3:2, 4:0, 5:1, 6:0, 7:1, 8:2, 9:1}}, 
-    "N":   {"real_capacity": np.full((6), 25.0), 
-            "reactive_capacity": np.zeros(6),
+    "N":   {"real_capacity": np.full((timestep_count), 25.0), 
+            "reactive_capacity": np.zeros(timestep_count),
             "real_cost": np.array([2, 0., 0., 2, 5.9, 0]),
             "installation_cost": 10000000,
             "unit_CO2": 2.9, 
             "count": 10,
             "per_node_limit": {node:10 for node in range(10)}}, 
-    "S":   {"real_capacity": np.array([0.065, 0.18, 0.235, 0.31, 0.325, 0.245]), 
-            "reactive_capacity": np.zeros(6),
+    "S":   {"real_capacity": np.zeros((timestep_count)), 
+            "reactive_capacity": np.zeros(timestep_count),
             "real_cost": np.array([2, 0., 0., 2, 50.4, 0]),
             "installation_cost": 444000,
             "unit_CO2": 8.5, 
             "count": 10,
             "per_node_limit": {node:5 for node in range(10)}},
-    "W":   {"real_capacity": np.array([5.374, 5.61, 5.612, 5.718, 5.31, 4.534]), 
-            "reactive_capacity": np.zeros(6),
+    "W":   {"real_capacity": np.zeros((timestep_count)), 
+            "reactive_capacity": np.zeros(timestep_count),
             "real_cost": np.array([2, 0., 0., 2, 10.6, 0]),
             "installation_cost": 1600000,
             "unit_CO2": 2.6, 
             "count": 10,
             "per_node_limit": {node:10 for node in range(10)}}
 }
+
+solar_wind_file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+    "data/solar_wind_output.csv"), "r")
+solar_wind_output = csv.reader(solar_wind_file)
+solar_wind = np.array([
+    timestep for timestep in itertools.islice(solar_wind_output, 
+        timestep_start, timestep_start + timestep_count)])
+solar_wind_file.close()
+solar_wind = solar_wind.astype(int) / 100
+gen_types["S"]["real_capacity"] = solar_wind[:, 0]
+gen_types["W"]["real_capacity"] = solar_wind[:, 1]
 
 for _, gen_profile in gen_types.items():
     assert (timestep_count == gen_profile["real_capacity"].shape[0]), "Demand profiles and generation profiles must specify the same number of timesteps"
