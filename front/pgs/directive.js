f4b4804b94bdaa12c8e7e5f46605604af63454a2
Annie <annie.shenglin.yang@gmail.com>
2018-03-21 22:16:11 -0400

    added unit for the evaluation result

--- front/pgs/directive.js
+++ front/pgs/directive.js
@@ -234,12 +234,6 @@ var challengesDirectiveController = ['$scope', '$rootScope', '$timeout', 'DataSe
 	$scope.goBack = function () {
 		$timeout(function () { $rootScope.$broadcast('pgsStateChanged', { state: 'login' }); });
 	}
-
-	var processChallenges = function () {
-		_.forEach($scope.challenges, function (c) { c.saved = c.saved_flag ? 'True' : 'False'; });
-	}
-
-	processChallenges();
 }]
 
 app.directive('challengeDirective', function () {
