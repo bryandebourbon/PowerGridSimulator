d021beadc40c1d2766856904ccee011f5ae64eae
Annie <annie.lafillette@gmail.com>
2018-04-10 15:09:02 -0400

    commented Guid component

--- front/pgs/Components/guid.js
+++ front/pgs/Components/guid.js
@@ -1,4 +1,15 @@
-var Guid = (function () {
+/* 
+** external: can be triggered from external sources
+** internal: only accessible from within the library
+*/
+
+/*  
+**	tag: external
+**	use: generate guid
+**	behavior: generate guid
+**	input: none
+**	output: { @generate }
+*/var Guid = (function () {
     var guid = function () {
         var s4 = function () {
             return Math.floor((1 + Math.random()) * 0x10000)
@@ -9,6 +20,7 @@ var Guid = (function () {
         return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 
+    /* functions exposed from Guid library to the external */
     return {
         generate: function () { return guid(); }
     }
