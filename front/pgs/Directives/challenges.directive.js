0376e2abfcc31236e049d5b7d958649d4fe370c7
Annie <annie.lafillette@gmail.com>
2018-04-10 22:07:00 -0400

    commented challenges.directive.js

--- front/pgs/Directives/challenges.directive.js
+++ front/pgs/Directives/challenges.directive.js
@@ -5,42 +5,66 @@ app.directive('challengesDirective', function () {
         scope: {
             challenges: '=?'
         },
-        controller: challengesDirectiveController
+        controller: ['$scope', '$rootScope', '$timeout', 'DataService', function ($scope, $rootScope, $timeout, $DataService) {
+            /*  
+            **	use: hit preview button for challenge entries
+            **	behavior: pop up modal showing challenge description
+            **	input: none
+            **	output: none
+            */
+            $scope.previewChallenge = function (id) {
+                var challenge = _.find($scope.challenges, function (c) { return c.id == id; });
+
+                if (challenge) {
+                    var _previewModalTitle = $('.modal-title');
+                    var _previewModalDescription = $('.modal-description');
+
+                    _previewModalTitle.text(challenge.name);
+                    _previewModalDescription.text(challenge.description);
+                }
+            }
+            /*  
+            **	use: hit simulate button for challenge entries
+            **	behavior: bring user to challenge simulation view
+            **	input: none
+            **	output: none
+            */
+            $scope.simulateChallenge = function (id) {
+                var challenge = _.find($scope.challenges, function (c) { return c.id == id; });
+
+                if (challenge) {
+                    /* retrieve single challenge definition and bring to challenge simulation view */
+                    $DataService.getChallenge({ teamname: $.cookie('teamname'), challengeID: id })
+                        .then(function (data) {
+                            Spinner.hide();
+
+                            $rootScope.$broadcast('pgsStateChanged', { state: 'grid', challenge: data });
+                        }).catch(function (error) {
+                            Spinner.hide();
+
+                            Warning.show(error);
+                        })
+                }
+            }
+
+            /*  
+            **	use: hit go back button
+            **	behavior: bring user back to log in page
+            **	input: none
+            **	output: none
+            */
+            $scope.goBack = function () {
+                $timeout(function () { $rootScope.$broadcast('pgsStateChanged', { state: 'login' }); });
+            }
+            /*  
+            **	use: hit log out button
+            **	behavior: bring user back to log in page
+            **	input: none
+            **	output: none
+            */
+            $scope.logOut = function () {
+                $timeout(function () { $rootScope.$broadcast('pgsStateChanged', { state: 'login' }); });
+            }
+        }]
     }
 })
-var challengesDirectiveController = ['$scope', '$rootScope', '$timeout', 'DataService', function ($scope, $rootScope, $timeout, $DataService) {
-    $scope.previewChallenge = function (id) {
-        var challenge = _.find($scope.challenges, function (c) { return c.id == id; });
-
-        if (challenge) {
-            var _previewModalTitle = $('.modal-title');
-            var _previewModalDescription = $('.modal-description');
-
-            _previewModalTitle.text(challenge.name);
-            _previewModalDescription.text(challenge.description);
-        }
-    }
-    $scope.simulateChallenge = function (id) {
-        var challenge = _.find($scope.challenges, function (c) { return c.id == id; });
-
-        if (challenge) {
-            $DataService.getChallenge({ teamname: $.cookie('teamname'), challengeID: id })
-                .then(function (data) {
-                    Spinner.hide();
-
-                    $rootScope.$broadcast('pgsStateChanged', { state: 'grid', challenge: data });
-                }).catch(function (error) {
-                    Spinner.hide();
-
-                    Warning.show(error);
-                })
-        }
-    }
-
-    $scope.goBack = function () {
-        $timeout(function () { $rootScope.$broadcast('pgsStateChanged', { state: 'login' }); });
-    }
-    $scope.logOut = function () {
-        $timeout(function () { $rootScope.$broadcast('pgsStateChanged', { state: 'login' }); });
-    }
-}]
